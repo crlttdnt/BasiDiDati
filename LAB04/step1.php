79fda70823ec6e108701b05730eb40a7986e8003
crlttdnt <carlotta.donato03@gmail.com>
2024-04-19 08:41:07 +0200

    lab04

---
+++ LAB04/step1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Basi di Dati | Laboratorio 3 </title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description" content="" />
+    <link rel="icon" href="favicon.png">
+</head>
+
+
+    <body>
+        <h3> I tuoi dati: </h3>
+
+        <?php 
+
+            if(isset($_SESSION["logged_user"])) {    //logged user che ho usato in login1 da user di login
+                //The isset function in PHP is used to determine whether a variable is set or not.
+                # Corretto
+                $user = $_SESSION["logged_user"];
+                echo "Utente $user le tue credenziali sono valide<br>";
+            } else {
+                # Non corretto
+                echo 'Devi fare il ' . '<a href= "login.html" >login</a>' . ' per accedere. <br>';
+            }
+
+        ?>
+        
+
+
+
+    </body>
+
+</html>
